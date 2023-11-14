fe18c6ba55cd9010c8727e586983280657713f05
Asim Thaha <iamasimthaha@gmail.com>
2023-11-14 19:13:56 +0530

    added api data

--- src/components/EmployeeAdd.jsx
+++ src/components/EmployeeAdd.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Employeenavbar from "./Employeenavbar";
+import axios from "axios";
 
 const EmployeeAdd = () => {
+  const [inputField, setInputField] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    phone: "",
+    organization: "",
+    designation: "",
+    salary: "",
+  });
+
+  const inputHandler = (event) => {
+    setInputField({ ...inputField, [event.target.name]: event.target.value });
+  };
+
+  const readVal = () => {
+    console.log(inputField);
+    axios
+      .post("http://172.22.2.251:4001/api/v1/packages", inputField)
+      .then((response) => {
+        alert(response.data.message);
+      });
+  };
   return (
     <div>
       <Employeenavbar />
@@ -11,42 +34,94 @@ const EmployeeAdd = () => {
             <div className="row g-3">
               <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                 <label htmlFor="" className="form-label">
-                  Employee Id
+                  First Name
                 </label>
-                <input type="text" className="form-control" />
+                <input
+                  type="text"
+                  className="form-control"
+                  name="first_name"
+                  value={inputField.first_name}
+                  onChange={inputHandler}
+                />
               </div>
               <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                 <label htmlFor="" className="form-label">
-                  Name
+                  Last Name
                 </label>
-                <input type="text" className="form-control" />
+                <input
+                  type="text"
+                  className="form-control"
+                  name="last_name"
+                  value={inputField.last_name}
+                  onChange={inputHandler}
+                />
               </div>
               <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                 <label htmlFor="" className="form-label">
                   phone
                 </label>
-                <input type="text" className="form-control" />
+                <input
+                  type="number"
+                  className="form-control"
+                  name="phone"
+                  value={inputField.phone}
+                  onChange={inputHandler}
+                />
               </div>
               <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                 <label htmlFor="" className="form-label">
                   email
                 </label>
-                <input type="email" className="form-control" />
+                <input
+                  type="email"
+                  className="form-control"
+                  name="email"
+                  value={inputField.email}
+                  onChange={inputHandler}
+                />
               </div>
               <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                 <label htmlFor="" className="form-label">
-                  Password
+                  Organisation
                 </label>
-                <input type="password" className="form-control" />
+                <input
+                  type="text"
+                  className="form-control"
+                  name="organization"
+                  value={inputField.organization}
+                  onChange={inputHandler}
+                />
               </div>
               <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                 <label htmlFor="" className="form-label">
-                  Confirm Password
+                  Designation
                 </label>
-                <input type="password" className="form-control" />
+                <input
+                  type=""
+                  className="form-control"
+                  name="designation"
+                  value={inputField.designation}
+                  onChange={inputHandler}
+                />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label htmlFor="" className="form-label">
+                  Salary
+                </label>
+                <input
+                  type=""
+                  className="form-control"
+                  name="salary"
+                  value={inputField.salary}
+                  onChange={inputHandler}
+                />
               </div>
               <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                <button type="submit" className="btn btn-primary">
+                <button
+                  onClick={readVal}
+                  type="submit"
+                  className="btn btn-primary"
+                >
                   REGISTER
                 </button>
               </div>
